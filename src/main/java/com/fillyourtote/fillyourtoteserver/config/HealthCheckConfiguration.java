de52a79cc4c1c7c5acbb71ae19c779d72c834fa2
Annie Rong <annierong302@gmail.com>
2025-09-03 20:31:41 -0400

    feat: add health check config

---
+++ src/main/java/com/fillyourtote/fillyourtoteserver/config/HealthCheckConfiguration.java
@@ -0,0 +1,9 @@
+package com.fillyourtote.fillyourtoteserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("prod")
+public class HealthCheckConfiguration {
+}
